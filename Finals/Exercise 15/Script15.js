7d973e467cca85a07d9226f3ea11b03491894580
Ysabel Dominique G. Mendoza <97865247+YsabelDominique@users.noreply.github.com>
2022-05-10 18:26:47 +0800

    Add files via upload

---
+++ Finals/Exercise 15/Script15.js
@@ -0,0 +1,20 @@
+var myName="Ysabel Dominique"
+var myAge="19"
+var myPhoneNumber="09234795083"
+var myEmail="20180472@s.ubauio.edu"
+var myAddress="Lower Padre Burgos, Baguio City"
+var myProgram="Computer Science"
+var myID="20180472"
+var myYear="1rst Year"
+var mySection="IAA1"
+
+console.log('Hello');
+console.log('First Name: ' + myName);
+console.log('Age: ' + myAge);
+console.log('Phone Number: ' + myPhoneNumber);
+console.log('Email Address: ' + myEmail);
+console.log('Mailing Address: ' + myAddress);
+console.log('Program: ' + myProgram);
+console.log('ID Number: ' + myID);
+console.log('Year Level: ' + myYear);
+console.log('Section: ' + mySection);
