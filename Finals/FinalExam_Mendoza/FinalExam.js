6e8490645dbd63f42bd9f7f3323db05349dabe42
Ysabel Dominique G. Mendoza <97865247+YsabelDominique@users.noreply.github.com>
2022-05-12 16:26:21 +0800

    Add files via upload

---
+++ Finals/FinalExam_Mendoza/FinalExam.js
@@ -0,0 +1,22 @@
+const myName="Ysabel Dominique Guieb Mendoza"
+const myEmail="20180472@s.ubaguio.edu"
+const myProgram="Computer Science"
+const myID="20180472"
+const myYear="1rst Year"
+const mySection="IAA1"
+
+console.log('Hello');
+console.log('Name: ' + myName);
+console.log('Email Address: ' + myEmail);
+console.log('Program: ' + myProgram);
+console.log('ID Number: ' + myID);
+console.log('Year Level: ' + myYear);
+console.log('Section: ' + mySection);
+
+var myWebsite="Sabby Angie Events"
+var founder="Ysabel Dominique Mendoza"
+
+console.log('Final Project/Final Exam');
+console.log('Website Name: ' + myWebsite);
+console.log('Founder: ' + founder);
+
